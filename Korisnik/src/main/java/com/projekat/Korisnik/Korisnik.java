03f840a3fefe3de8977592ed68e16c83feb31e80
asijah1 <asijah1@etf.unsa.ba>
2020-03-27 15:35:11 +0100

    bilo je malih problema sa Korisnik/pom.xml - ispravljeno

--- Korisnik/src/main/java/com/projekat/Korisnik/Korisnik.java
+++ Korisnik/src/main/java/com/projekat/Korisnik/Korisnik.java
@@ -7,8 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-<<<<<<< HEAD
-=======
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -16,7 +14,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import javax.validation.constraints.Email;
->>>>>>> 8eb88351d19ae71f2ee25036a72c7e326dbd0600
 
 @Entity
 public class Korisnik {
@@ -35,16 +32,8 @@ public class Korisnik {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-<<<<<<< HEAD
     
     private String firstName;
-=======
-	
-	@NotNull(message = "First name cannot be null")
-	private String firstName;
-	
-	@NotNull(message = "Last name cannot be null")
->>>>>>> 8eb88351d19ae71f2ee25036a72c7e326dbd0600
     private String lastName;
 	
 	@Size(min = 3, max = 50, message = "Location characters length has to be inbetween (3,50)")
