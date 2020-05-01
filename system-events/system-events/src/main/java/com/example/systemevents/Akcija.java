aa063f4c5ff37dfa5b0b01d030b6f7bbb0cca607
Irhad Omanovic <irhadomanovic@hotmail.com>
2020-05-01 17:11:47 +0200

    Dodana klasa

---
+++ system-events/system-events/src/main/java/com/example/systemevents/Akcija.java
@@ -0,0 +1,87 @@
+package com.example.systemevents;
+
+import java.util.Date;
+
+public class Akcija {
+	
+	enum TipOdgovora {
+		USPJESNA,
+		GRESKA
+	}
+	
+	enum TipAkcije {
+		GET,
+		UPDATE,
+		CREATE,
+		DELETE
+	};
+	
+	private Date timestampAkcije;
+	private String mikroservis;
+	private String korisnik;
+	private TipAkcije akcija;
+	private String resurs;
+	private TipOdgovora odgovor;
+	
+	
+	public Akcija(Date timestampAkcije, String mikroservis, String korisnik, TipAkcije akcija, String resurs,
+			TipOdgovora odgovor) {
+		super();
+		this.timestampAkcije = timestampAkcije;
+		this.mikroservis = mikroservis;
+		this.korisnik = korisnik;
+		this.akcija = akcija;
+		this.resurs = resurs;
+		this.odgovor = odgovor;
+	}
+	
+	
+	public Date getTimestampAkcije() {
+		return timestampAkcije;
+	}
+	public void setTimestampAkcije(Date timestampAkcije) {
+		this.timestampAkcije = timestampAkcije;
+	}
+	
+	
+	public String getMikroservis() {
+		return mikroservis;
+	}
+	public void setMikroservis(String mikroservis) {
+		this.mikroservis = mikroservis;
+	}
+	
+	
+	public String getKorisnik() {
+		return korisnik;
+	}
+	public void setKorisnik(String korisnik) {
+		this.korisnik = korisnik;
+	}
+	
+	
+	public TipAkcije getAkcija() {
+		return akcija;
+	}
+	public void setAkcija(TipAkcije akcija) {
+		this.akcija = akcija;
+	}
+	
+	
+	public String getResurs() {
+		return resurs;
+	}
+	public void setResurs(String resurs) {
+		this.resurs = resurs;
+	}
+	
+	
+	public TipOdgovora getOdgovor() {
+		return odgovor;
+	}
+	public void setOdgovor(TipOdgovora odgovor) {
+		this.odgovor = odgovor;
+	}
+	
+	
+}
