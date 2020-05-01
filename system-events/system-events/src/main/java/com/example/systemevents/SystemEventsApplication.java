1ddc3b40e688d003963f0e0982fcf73f509bc13d
asijah1 <asijah1@etf.unsa.ba>
2020-05-01 22:20:12 +0200

    podešen EventApplication i override-ana metoda start (metoda iz .proto fajla)

--- system-events/system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
+++ system-events/system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
@@ -9,5 +9,21 @@ public class SystemEventsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SystemEventsApplication.class, args);
 	}
+	
+	/*
+	@SpringBootApplication
+	public class SystemEventsApplication {
+		//grpc server
+		public static void main(String[] args) throws IOException, InterruptedException{
+	        Server server = ServerBuilder
+	          .forPort(8083)
+	          .addService(new SystemEventsImpl()).build();
+			
+	        server.start();
+	        server.awaitTermination();
+	    }
+	
+}
+	 */
 
 }
---
+++ system-events/system-events/src/main/java/com/example/systemevents/SystemEventsImpl.java
@@ -0,0 +1,26 @@
+package com.example.systemevents;
+
+public class SystemEventsImpl /*extends SystemEventsImplBase*/ {
+
+	/*
+	@Override
+	public void start(Request request, StreamObserver<Response> responseObserver) {
+ 
+        String odgovorNaAkciju = new StringBuilder()
+          .append("Servis, ")
+          .append(request.getTimestamp())
+          .append(request.getNazivMikroservisa())
+          .append(request.getTip())
+          .append(request.getNazivResursa())
+          .toString();
+ 
+        Response response = Response.newBuilder()
+          .setOdgovorNaAkciju(odgovorNaAkciju)
+          .build();
+ 
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+	 */
+	
+}
