f8ee4c1f86e6f6c117b0ff834eb5436d52e900a2
asijah1 <asijah1@etf.unsa.ba>
2020-05-06 19:56:51 +0200

    Sinhronizacija ApiGateway-a sa eurekom

--- APIGateway/src/main/java/com/Projekat/APIService/APIService.java
+++ APIGateway/src/main/java/com/Projekat/APIService/APIService.java
@@ -1,12 +1,16 @@
 package com.Projekat.APIService;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
-import com.projekat.Katalog.model.Ponuda;
+import com.Projekat.model.Ponuda;
+import com.Projekat.APIGateway.ApiGatewayApplication;
 
 public class APIService {
-	
+	/*
 	public List dohvatiPonude(Long id) {
 		ResponseEntity<Ponuda[]> response = restTemplate.getForEntity("http://ponuda/ponude/katalogSaId?id=" + id, Ponuda[].class);
 	}
+	*/
 }
