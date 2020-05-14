3b0741bfa040ff7e348e09770051545d82629190
Irhad Omanovic <iomanovic1@etf.unsa.ba>
2020-05-14 14:00:15 +0200

    Rad na autentikaciji

--- APIGateway/src/main/java/com/Projekat/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/Projekat/APIGateway/ApiGatewayApplication.java
@@ -1,5 +1,8 @@
 package com.Projekat.APIGateway;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,14 +10,36 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @EnableZuulProxy
 @SpringBootApplication
+@RestController
+@EnableResourceServer
+@EnableAuthorizationServer
 @EnableDiscoveryClient
 @EnableEurekaClient
 public class ApiGatewayApplication {
-
+	
+	@RequestMapping(value = { "/user" }, produces = "application/json")
+	public Map<String, Object> user(OAuth2Authentication user) {
+	    Map<String, Object> userInfo = new HashMap<>();
+	    userInfo.put(
+	           "user", 
+	            user.getUserAuthentication().getPrincipal());
+	    userInfo.put("authorities",
+	       AuthorityUtils.authorityListToSet(
+	       user.getUserAuthentication()
+	           .getAuthorities()));
+	    return userInfo;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
@@ -24,4 +49,5 @@ public class ApiGatewayApplication {
 	RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+	
 }
