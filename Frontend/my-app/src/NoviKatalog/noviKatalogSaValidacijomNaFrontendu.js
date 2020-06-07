fd857c6a4d8906c8ea0ce468958a55c7bf9c8c1c
Amir Sijah <amir.sijah@elreg.ba>
2020-06-07 10:06:25 +0200

    validacija komponente dodajKatalog

---
+++ Frontend/my-app/src/NoviKatalog/noviKatalogSaValidacijomNaFrontendu.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+//import './noviKatalog.css';
+
+const katalogAPI = "http://localhost:8087/katalozi"
+
+export class noviKatalog extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        katalog: {
+            nazivProizvoda: '',
+            lokacija: '',
+            povrsina: '',
+            cijena: '',
+            datumObjave:'',
+            datumZavrsetka:''
+        },
+        submitted: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+handleChange(event) {
+    const { name, value } = event.target;
+    const { katalog } = this.state;
+    this.setState({
+        katalog: {
+            ...katalog,
+            [name]: value
+        }
+    });
+}
+
+handleSubmit(event) {
+    event.preventDefault();
+
+    this.setState({ submitted: true });
+    const { katalog } = this.state;
+
+    const text = {
+      nazivProizvoda: this.state.nazivProizvoda,
+      lokacija: this.state.lokacija,
+      povrsina: this.state.povrsina,
+      cijena: this.state.cijena,
+      datumObjave: this.state.datumObjave,
+      datumZavrsetka: this.state.datumZavrsetka
+  }
+  
+  console.log(JSON.stringify(text));
+
+    if (katalog.nazivProizvoda && katalog.lokacija && katalog.povrsina && katalog.cijena, katalog.datumObjave, katalog.datumZavrsetka) {
+        //ovdje ide dio sa rgistracijom kataloga
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', katalogAPI);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xhr.send(JSON.stringify(text));
+    }
+
+}
+
+  render() {
+    const { katalog, submitted } = this.state;
+    return (
+      	<div class="container">
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
+          	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+          	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+          	<h2>Objavi katalog</h2>
+            <form name="form" onSubmit={this.handleSubmit}>
+              <div className={'form-group' + (submitted && !katalog.nazivProizvoda ? ' has-error' : '')}>
+                <label for="nazivProizvoda">Naziv proizvoda: </label>
+                <input type="text" class="form-control" name="nazivProizvoda" onChange={this.handleChange}/>
+                {submitted && !katalog.nazivProizvoda &&
+                        <div className="help-block">Name of product is required</div>
+                    }
+              </div>
+              <div className={'form-group' + (submitted && !katalog.lokacija ? ' has-error' : '')}>
+                <label for="lokacija">Lokacija: </label>
+                <select class="form-control" name="lokacija" onChange={this.handleChange}>
+                  <option value="Travnik">Travnik</option>
+                  <option value="Sarajevo">Sarajevo</option>
+                  <option selected value="Visoko">Visoko</option>
+                  <option value="Kakanj">Kakanj</option>
+                </select>
+                {submitted && !katalog.lokacija &&
+                        <div className="help-block">Location of product is required</div>
+                    }
+              </div>
+              <div className={'form-group' + (submitted && !katalog.povrsina ? ' has-error' : '')}>
+                <label for="povrsina">Povrsina (m^2): </label>
+                <input type="text" class="form-control" id="povrsina" placeholder="unesite povrsinu" name="povrsina" onChange={this.handleChange}/>
+                {submitted && !katalog.povrsina &&
+                        <div className="help-block">Surface is required</div>
+                    }
+              </div>
+              <div className={'form-group' + (submitted && !katalog.cijena ? ' has-error' : '')}>
+                <label for="cijena">Cijena (KM): </label>
+                <input type="text" class="form-control" id="cijena" placeholder="unesite cijenu" name="cijena" onChange={this.handleChange}/>
+                {submitted && !katalog.cijena &&
+                        <div className="help-block">Price of product is required</div>
+                    }
+              </div>
+              <div className={'form-group' + (submitted && !katalog.datumObjave ? ' has-error' : '')}>
+                <label for="datumObjave">Datum objave:</label>
+                <input type="date" class="form-control" id="datumObjave" placeholder="odaberite aktuelni datum" name="datumObjave" onChange={this.handleChange}/>
+                {submitted && !katalog.datumObjave &&
+                        <div className="help-block">Date of publication is required</div>
+                    }
+              </div>
+              <div className={'form-group' + (submitted && !katalog.datumZavrsetka ? ' has-error' : '')}>
+                <label for="datumZavrsetka">Datum zavrsetka:</label>
+                <input type="date" class="form-control" id="datumZavrsetka" placeholder="odaberite datum zavrsetka" name="datumZavrsetka" onChange={this.handleChange}/>
+                {submitted && !katalog.datumZavrsetka &&
+                        <div className="help-block">End date is required</div>
+                    }
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary">Objavi</button>
+                <Link to="/pocetna" className="btn btn-link">Cancel</Link>
+              </div>
+            </form>
+        </div>
+    )
+  }
+}
+
+
+
+export default noviKatalog;
